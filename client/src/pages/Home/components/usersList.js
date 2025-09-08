0bda87280b55e47131d8c34e751f189fc2021f5c
Pradip Shukla <pradipshukla12699@gmail.com>
2025-09-08 17:53:38 +0530

    start new chat done!

--- client/src/pages/Home/components/usersList.js
+++ client/src/pages/Home/components/usersList.js
@@ -1,7 +1,31 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import {toast} from 'react-hot-toast'
+import {createNewChat} from './../../../apiCalls/chat';
+import { hideLoader, showLoader } from "../../../redux/loaderSlice";
+import { setAllchats } from "../../../redux/usersSlice";
 
 function UsersList({searchKey}) {
-    const {allUsers, allChats} = useSelector((state) => state.userReducer);
+    const dispatch = useDispatch();
+    const {allUsers, allChats, user: currentUser} = useSelector((state) => state.userReducer);
+
+    const startNewChat = async (searchedUserId) => {
+        let response;
+        try {
+            dispatch(showLoader());
+            response = await createNewChat([currentUser._id, searchedUserId]);
+            dispatch(hideLoader());
+
+            if(response.success) {
+                toast.success(response.message);
+                const newChat = response.data;
+                const updatedChat = [...allChats, newChat];
+                dispatch(setAllchats(updatedChat));
+            }
+        } catch (error) {
+            dispatch(hideLoader());
+            toast.error(response.message);
+        }
+    }
 
     return (
         allUsers.filter(user => (
@@ -28,7 +52,10 @@ function UsersList({searchKey}) {
                             {
                                 !allChats.find(chat => chat.members.includes(user._id)) &&
                                 <div className="user-start-chat">
-                                    <button className="user-start-chat-btn">Start Chat</button>
+                                    <button className="user-start-chat-btn" 
+                                        onClick={() => {startNewChat(user._id)}}>
+                                        Start Chat
+                                    </button>
                                 </div>
                             }
                         </div>
