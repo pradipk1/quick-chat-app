4eec52e3989f2dd356aba031550bb5131a8d05c7
Pradip Shukla <pradipshukla12699@gmail.com>
2025-09-09 16:05:42 +0530

    displaying messages in chat area done!

--- client/src/pages/Home/components/chat.js
+++ client/src/pages/Home/components/chat.js
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {createNewMessage} from './../../../apiCalls/message';
+import {createNewMessage, getAllMessages} from './../../../apiCalls/message';
 import {toast} from 'react-hot-toast';
 import {showLoader, hideLoader} from './../../../redux/loaderSlice';
 
@@ -9,6 +9,7 @@ function ChatArea() {
     const { selectedChat, user: currentUser } = useSelector(state => state.userReducer);
     const selectedUser = selectedChat.members.find(member => member._id !== currentUser._id);
     const [message, setMessage] = useState('');
+    const [allMessages, setAllMessages] = useState([]);
 
     const sendMesage = async () => {
         let response;
@@ -32,13 +33,43 @@ function ChatArea() {
         }
     }
 
+    const getMessages = async () => {
+        try {
+            dispatch(showLoader());
+            const response = await getAllMessages(selectedChat._id);
+            dispatch(hideLoader());
+
+            if(response.success) {
+                setAllMessages(response.data);
+            }
+        } catch (error) {
+            dispatch(hideLoader());
+            toast.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getMessages();
+    }, [selectedChat]);
+
     return (
         <div className="app-chat-area">
             <div className="app-chat-area-header">
                 {selectedUser.firstname + ' ' + selectedUser.lastname}
             </div>
             <div className='main-chat-area'>
-                CHAT AREA
+                {
+                    allMessages.map(msg => {
+                        const isCurrentUserSender = msg.sender === currentUser._id;
+                        return <div className="message-container" 
+                                style={isCurrentUserSender ? {justifyContent: 'end'} : {justifyContent: 'start'}}
+                            >
+                            <div className={isCurrentUserSender ? "send-message" : "received-message"}>
+                                {msg.text}
+                            </div>
+                        </div>
+                    })
+                }
             </div>
             <div className="send-message-div">
                 <input type="text" 
