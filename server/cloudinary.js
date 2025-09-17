8ca069556b0fbdfc805eb1e59fc904513b259394
Pradip Shukla <pradipshukla12699@gmail.com>
2025-09-17 23:13:22 +0530

    cloudinary.js file deleted

--- server/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary');
-
-cloudinary.config({ 
-  cloud_name: 'do2ln8ph3', 
-  api_key: '325124256382463', 
-  api_secret: '0kwyFhmWksIjukSsUNVoFUyjgK8'
-});
-
-module.exports = cloudinary;
